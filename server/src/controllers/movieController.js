b0c82040f560b74aa6cc8a993aa098d5a61c8470
OnlineBunker <yash.dhankhar06@gmail.com>
2025-11-18 12:44:11 +0530

    movies backend completed

---
+++ server/src/controllers/movieController.js
@@ -0,0 +1,44 @@
+const axios = require("axios");
+
+const TMDB_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = process.env.TMDB_API_KEY;
+
+const searchMovies = async (req, res) => {
+    try {
+        const query = req.query.query;
+
+        if (!query) {
+            return res.status(400).json({ message: "Query is required" });
+        }
+
+        const response = await axios.get(`${TMDB_BASE_URL}/search/movie`, {
+            params: {
+                api_key: API_KEY,
+                query,
+            },
+        });
+        res.status(200).json(response.data);
+
+    } catch (error) {
+        res.status(500).json({ message: "TMDB search failed" });
+    }
+}
+
+const getMovieDetails = async (req, res) => {
+  try {
+    const tmdbId = req.params.tmdbId;
+
+    const response = await axios.get(`${TMDB_BASE_URL}/movie/${tmdbId}`, {
+      params: {
+        api_key: API_KEY,
+        append_to_response: "videos,images,credits",
+      },
+    });
+
+    res.status(200).json(response.data);
+  } catch (err) {
+    res.status(500).json({ message: "TMDB details fetch failed" });
+  }
+};
+
+module.exports = { searchMovies, getMovieDetails };
--- server/src/index.js
+++ server/src/index.js
@@ -9,10 +9,12 @@ app.use(express.urlencoded({ extended: true }));
 const authRoutes = require("./routes/authRoutes");
 const reviewRoutes = require("./routes/reviewRoutes");
 const watchlistRoutes = require("./routes/watchlistRoutes");
+const movieRoutes = require("./routes/movieRoutes");
 
 app.use("/api/auth", authRoutes);
 app.use("/api/reviews", reviewRoutes);
 app.use("/api/watchlist", watchlistRoutes);
+app.use("/api/movies", movieRoutes);
 
 const PORT = process.env.PORT || 3000;
 

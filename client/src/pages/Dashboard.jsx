411db24b566d3bbebaf373b200ef691e4fd8daf4
OnlineBunker <yash.dhankhar06@gmail.com>
2025-11-21 21:36:02 +0530

    renamed error-2

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
-import '../styles/Dashboard.css';
+import '../styles/dashboard.css';
 
 const Dashboard = () => {
   const { user } = useAuth();
